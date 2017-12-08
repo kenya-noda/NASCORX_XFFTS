8dbb0829df7fda450cf471f6b3369c67c8fba79c
tetsu <inaba.t@a.phys.nagoya-u.ac.jp>
2017-12-09 02:10:15 +0900

    modified import

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
 
-import NASCO_XFFTS.xffts_udp_client as xffts_udp_client
-import NASCO_XFFTS.xffts_data_client as xffts_data_client
-import NASCO_XFFTS.xffts_data_server as xffts_data_server
+import NASCO_XFFTS.xffts_udp_client
+import NASCO_XFFTS.xffts_data_client
+import NASCO_XFFTS.xffts_data_server
