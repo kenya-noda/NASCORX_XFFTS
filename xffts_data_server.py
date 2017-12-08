0919a11895b039c7f97c9f2a2e79c349c1978134
tetsu <inaba.t@a.phys.nagoya-u.ac.jp>
2017-12-09 02:25:34 +0900

    modified import

--- xffts_data_server.py
+++ xffts_data_server.py
@@ -8,7 +8,7 @@ import socket
 import struct
 import signal
 import threading
-import NASCO_XFFTS.xffts_udp_client
+import NASCO_XFFTS.xffts_udp_client as
 
 from necst.msg import XFFTS_msg
 from necst.msg import XFFTS_pm_msg
